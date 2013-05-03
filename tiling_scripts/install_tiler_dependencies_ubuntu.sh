de8d134c3ef4f8a2fc08c69458214ca43937a018
Brian Dunnette <dunn0172@umn.edu>
2013-05-03 09:50:45 -0500

    script to automatically install tiler dependencies on Ubuntu

---
+++ tiling_scripts/install_tiler_dependencies_ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libvips-dev libtiff5-dev libjpeg-dev openslide-tools imagemagick
+wget http://www.vips.ecs.soton.ac.uk/supported/current/vips-7.32.3.tar.gz
+tar -zxf vips-7.32.3.tar.gz
+cd vips-7.32.3
+./configure
+make
+sudo make install
