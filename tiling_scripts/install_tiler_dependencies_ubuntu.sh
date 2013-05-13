6207c8c7a077baaa2a87c76a5ab53812f13488a7
Brian D <brian@dunnette.us>
2013-05-13 14:17:14 -0500

    Update install_tiler_dependencies_ubuntu.sh

--- tiling_scripts/install_tiler_dependencies_ubuntu.sh
+++ tiling_scripts/install_tiler_dependencies_ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libvips-dev libtiff5-dev libjpeg-dev openslide-tools imagemagick
+sudo apt-get install libvips-dev libtiff5-dev libtiff-tools libjpeg-dev openslide-tools imagemagick
 wget http://www.vips.ecs.soton.ac.uk/supported/current/vips-7.32.3.tar.gz
 tar -zxf vips-7.32.3.tar.gz
 cd vips-7.32.3
